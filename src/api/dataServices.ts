8d141af5b66a7082e7807d0b3b7ebe53c7f87dc5
Brian Mthembu <Brian.Mthembu@imqs.co.za>
2026-07-23 12:35:49 +0200

    Error handling on fetch methods

--- src/api/dataServices.ts
+++ src/api/dataServices.ts
@@ -1,37 +1,35 @@
 const API_BASE = 'http://localhost:2050';
 
 export async function fetchTotalRecords(): Promise<number> {
-    try {
-		let url = `${API_BASE}/recordCount`;
-    	let count = await $.get(url);
-    	return Number(count);
-	}catch(error) {
-		handleApiError('/recordCount', error);
-	}
+    let url = `${API_BASE}/recordCount`;
+
+	return $.get(url)
+		.then(count => Number(count))
+		.catch(error => handleApiError('/recordCount', error));
 }
 
 export async function fetchColumns(): Promise<string[]> {
-    try {
-		let url = `${API_BASE}/columns`;
-		let cols = await $.get(url);
-		if (typeof cols === 'string')
-			return JSON.parse(cols);
-		return cols;
-	}catch(error) {
-		handleApiError('/columns', error);
-	}
+	let url = `${API_BASE}/columns`;
+
+	return $.get(url)
+		.then(cols => {
+			if(typeof cols === 'string')
+				return JSON.parse(cols);
+			return cols;
+		})
+		.catch(error => handleApiError('/columns', error));
 }
 
 export async function fetchCurrentPageRecords(from: number, to: number): Promise<any[][]> {
-    try {
-		let url = `${API_BASE}/records?from=${from}&to=${to}`;
-		let data = await $.get(url);
-		if (typeof data === 'string')
-			return JSON.parse(data);
-		return data;
-	}catch(error) {
-		handleApiError('/records', error);
-	}
+    let url = `${API_BASE}/records?from=${from}&to=${to}`;
+
+	return $.get(url)
+		.then(data => {
+			if(typeof data === 'string')
+				return JSON.parse(data);
+			return data;
+		})
+		.catch(error => handleApiError('/records', error));
 }
 
 function handleApiError(endpoint: string, error: any): never {
